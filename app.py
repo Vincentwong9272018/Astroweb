1a2f0f90c5f68d918cc979e3277e0b62d96bee52
Vincentwong9272018 <36910810+Vincentwong9272018@users.noreply.github.com>
2026-06-27 16:24:15 +0100

    Update session state initialization dates

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ st.set_page_config(page_title="專業星盤系統", layout="wide")
 now = datetime.datetime.now()
 if 'n_year' not in st.session_state:
     st.session_state.update({
-        'n_year': 1993, 'n_month': 9, 'n_day': 27, 'n_hour': 12, 'n_minute': 0, 'n_loc': "Manchester", 'name_input': "Vincent",
+        'n_year': 1990, 'n_month': 1, 'n_day': 1, 'n_hour': 12, 'n_minute': 0, 'n_loc': "Manchester", 'name_input': "Vincent",
         'p_year': now.year, 'p_month': now.month, 'p_day': now.day, 'p_hour': now.hour, 'p_minute': now.minute, 'p_loc': "Manchester",
-        'target_age': now.year - 1993
+        'target_age': now.year - 1990
     })
 
 def set_current_time():
